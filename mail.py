d84402e9ac1a3137cd66dd95eea0be76d571ef12
Georg Brandl <georg@python.org>
2010-09-09 09:26:44 +0200

    Untabify.

--- mail.py
+++ mail.py
@@ -10,51 +10,51 @@ CSET_URL = BASE + '%s/rev/%s'
 FROM = '%s <python-checkins@python.org>'
 
 def send(sub, sender, to, body):
-	msg = MIMEMultipart()
-	msg['Subject'] = sub
-	msg['To'] = to
-	msg['From'] = sender
-	msg.attach(MIMEText(body, _subtype='plain'))
-	smtp = smtplib.SMTP()
-	smtp.connect()
-	smtp.sendmail(sender, msg['To'], msg.as_string())
-	smtp.close()
+    msg = MIMEMultipart()
+    msg['Subject'] = sub
+    msg['To'] = to
+    msg['From'] = sender
+    msg.attach(MIMEText(body, _subtype='plain'))
+    smtp = smtplib.SMTP()
+    smtp.connect()
+    smtp.sendmail(sender, msg['To'], msg.as_string())
+    smtp.close()
 
 def incoming(ui, repo, **kwargs):
-	
-	displayer = cmdutil.changeset_printer(ui, repo, False, False, True)
-	ctx = repo[kwargs['node']]
-	displayer.show(ctx)
-	log = displayer.hunk[ctx.rev()]
-	user = os.environ.get('HGPUSHER', 'local')
-	path = '/'.join(repo.root.split('/')[4:])
-	
-	body = ['%s pushed %s to %s:' % (user, str(ctx), path), '']
-	body += [CSET_URL % (path, ctx)]
-	body += log.splitlines()[:-1]
-	body += ['files:       ' + ', '.join(ctx.files()), '']
-	
-	diffopts = patch.diffopts(repo.ui, {'git': True, 'showfunc': True})
-	parents = ctx.parents()
-	node1 = parents and parents[0].node() or nullid
-	node2 = ctx.node()
-	differ = patch.diff(repo, node1, node2, opts=diffopts)
-	body.append(''.join(chunk for chunk in differ))
-	
-	body.append('--')
-	body.append('Repository URL: %s%s' % (BASE, path))
 
-	to = ui.config('mail', 'notify', None)
-	if to is None:
-		print 'no email address configured'
-		return
-	
-	desc = ctx.description().splitlines()[0]
-	if len(desc) > 80:
-		desc = desc[:80]
-		if ' ' in desc:
-			desc = desc.rsplit(' ', 1)[0]
-	
-	subj = '%s: %s' % (path, desc)
-	send(subj, FROM % user, to, '\n'.join(body))
-	print 'notified %s of incoming changeset %s' % (to, ctx)
+    displayer = cmdutil.changeset_printer(ui, repo, False, False, True)
+    ctx = repo[kwargs['node']]
+    displayer.show(ctx)
+    log = displayer.hunk[ctx.rev()]
+    user = os.environ.get('HGPUSHER', 'local')
+    path = '/'.join(repo.root.split('/')[4:])
+
+    body = ['%s pushed %s to %s:' % (user, str(ctx), path), '']
+    body += [CSET_URL % (path, ctx)]
+    body += log.splitlines()[:-1]
+    body += ['files:       ' + ', '.join(ctx.files()), '']
+
+    diffopts = patch.diffopts(repo.ui, {'git': True, 'showfunc': True})
+    parents = ctx.parents()
+    node1 = parents and parents[0].node() or nullid
+    node2 = ctx.node()
+    differ = patch.diff(repo, node1, node2, opts=diffopts)
+    body.append(''.join(chunk for chunk in differ))
+
+    body.append('--')
+    body.append('Repository URL: %s%s' % (BASE, path))
+
+    to = ui.config('mail', 'notify', None)
+    if to is None:
+        print 'no email address configured'
+        return
+
+    desc = ctx.description().splitlines()[0]
+    if len(desc) > 80:
+        desc = desc[:80]
+        if ' ' in desc:
+            desc = desc.rsplit(' ', 1)[0]
+
+    subj = '%s: %s' % (path, desc)
+    send(subj, FROM % user, to, '\n'.join(body))
+    print 'notified %s of incoming changeset %s' % (to, ctx)
