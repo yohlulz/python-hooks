0e9a4bb3478b1816c29ce87f0c185be86076808b
Georg Brandl <georg@python.org>
2011-03-06 10:48:19 +0100

    Fix mistake in buildbot hook

--- hgbuildbot.py
+++ hgbuildbot.py
@@ -88,7 +88,7 @@ def hook(ui, repo, hooktype, node=None, source=None, **kwargs):
             # Explicitly compare current with its first parent (otherwise
             # some files might be "forgotten" if they are copied as-is from the
             # second parent).
-            p1 = repo[hex(p[0])]
+            p1 = repo[hex(parents[0])]
             modified, added, removed, deleted = repo.status(rev, p1)[:4]
             files = set()
             for l in (modified, added, removed, deleted):
